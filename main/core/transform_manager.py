ac5e9fa62ac89d9f4c3f68d7963abfd0ee4958f6
Alice <alicegillbanks@gmail.com>
2026-01-30 10:51:15 +0000

    oh dear its all going wrong

--- main/core/transform_manager.py
+++ main/core/transform_manager.py
@@ -12,7 +12,6 @@ class TransformManager:
     """
     def __init__(self, input_expression):
         self.transformations_queue = Queue(6)
-        self.original_function = input_expression
         self.current_function = input_expression
 
     def addTransformations(self, x_stretch_box, y_stretch_box, x_shift_box, y_shift_box, x_reflect, y_reflect):
@@ -23,26 +22,25 @@ class TransformManager:
             x_stretch_box, y_stretch_box, x_shift_box, y_shift_box, x_reflect, y_reflect
         )
 
-    def applyTransformation(self, transformation, update_base=True):
+    def applyTransformation(self, transformation):
         """
         Applies a given transformation to the current function
         """
-        modifier = None
+        new_func = None
 
         if transformation.type == "shift":
-            modifier = ShiftFunction(self.current_function, transformation.axis, transformation.value)
+            new_func = ShiftFunction(self.current_function, transformation.axis, transformation.value)
         elif transformation.type == "stretch":
-            modifier = StretchFunction(self.current_function, transformation.axis, transformation.value)
+            new_func = StretchFunction(self.current_function, transformation.axis, transformation.value)
         elif transformation.type == "reflect":
-            modifier = ReflectFunction(self.current_function, transformation.axis)
+            new_func = ReflectFunction(self.current_function, transformation.axis)
         elif transformation.type == "differentiate":
-            modifier = DifferentiateFunction(self.current_function)
+            new_func = DifferentiateFunction(self.current_function)
 
-        if modifier:
-            new_func = modifier.ModifyFunction()
-            if update_base:
-                self.current_function = new_func
+        if new_func:
             return new_func
+        else:
+            return None
 
     def applyAllTransformations(self, graph_plotter):
         """
@@ -56,51 +54,17 @@ class TransformManager:
         # Resets the queue once the transformations have been applied
         self.transformations_queue = Queue(6)
 
+    def setBaseFunction(self, new_function):
+        """
+        Sets the current function to the original function
+        """
+        self.current_function = new_function
+
     def getCurrentFunction(self):
         """
         Returns the current function
         """
         return self.current_function
-            
-    def nextTransformation(self):
-        """
-        Apply the next transformation in the queue and return the new function
-        """
-        if self.transformations_queue.isEmpty():
-            return None
-        
-        # Get the transformation
-        transformation = self.transformations_queue.dequeue()
-
-        # Create a copy of the current function to modify
-        ast_copy = copyAST(self.current_function.getFunction())
-        var_copy = self.current_function.getVariable()
-        new_func = Function(ast_copy, var_copy)
-
-        modifier = None
-        # Create an instance of the relevant modifier class 
-        if transformation.getType() == "shift":
-            modifier = ShiftFunction(new_func, transformation.getAxis(), transformation.getVal())
-        elif transformation.getType() == "stretch":
-            modifier = StretchFunction(new_func, transformation.getAxis(), transformation.getVal())
-        elif transformation.getType() == "reflect":
-            modifier = ReflectFunction(new_func, transformation.getAxis())
-        elif transformation.getType() == "differentiate":
-            modifier = DifferentiateFunction(new_func)
-
-        if modifier:
-            # Create a new function from the modifier and store as the current function
-            self.current_function = modifier.ModifyFunction()
-            return self.current_function
-        return None
-
-    def setBaseFunction(self, new_function):
-        """
-        Set a modified function as the new base function
-        This is for implementation in repeated differentiation for further derivatives
-        """
-        self.current_function = new_function
-        self.transformations_queue = Queue(1)
 
     def hasTransformations(self):
         return not self.transformations_queue.isEmpty()
